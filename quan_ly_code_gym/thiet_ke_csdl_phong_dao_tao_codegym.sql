f14ca067f88f4c4d7b082dd2fab15ca7193c8bd3
unknown <buivanluu1997@gmail.com>
2024-10-16 22:33:56 +0700

    nop_bai_tap

--- quan_ly_code_gym/thiet_ke_csdl_phong_dao_tao_codegym.sql
+++ quan_ly_code_gym/thiet_ke_csdl_phong_dao_tao_codegym.sql
@@ -140,5 +140,48 @@ inner join class on student.class_id = class.id
 where class.name = 'c1121g1'
 order by student.point desc;
 
+-- Bài 4: Các hàm thông dụng trong CSDL
+-- 1.Hiện thị danh sách các lớp có học viên theo học và số lượng học viên của mỗi lớp
+select c.name, count(s.id) as student_count
+from class c
+left join student s on c.id = s.class_id
+group by c.name;
+
+-- 2. Tìm điểm lớn nhất của mỗi lớp
+select c.name, max(s.point) as max_point
+from class c
+left join student s on c.id = s.class_id
+group by c.name;
+
+-- 3. Tình điểm trung bình của từng lớp
+select c.name, avg (s.point) as average_point
+from class c
+left join student s on c.id = s.class_id
+group by c.name;
+
+-- 4. Lấy ra toàn bộ tên và ngày sinh các instructor và student ở CodeGym.
+select name,birthday from student
+union
+select name,birthday from instructor;
+
+-- 5. Lấy ra top 3 học viên có điểm cao nhất của trung tâm.
+select s.name, s.point 
+from student s
+order by s.point desc
+limit 3;
+
+-- 6. Lấy ra các học viên có điểm số là cao nhất của trung tâm.
+select name, point from student
+where point = (select max(point) from student);
+
+-- 7. lấy ra tất cả các giảng viên chưa từng tham gia giảng dạy
+select i.* from instructor i
+left join instructor_class ic on i.id = ic.instructor_id
+where ic.instructor_id is null;
+
+
+
+
+
 
   
