4a30bcb51197a4fe2b85a6046dc12a2f16b8827e
unknown <buivanluu1997@gmail.com>
2024-10-18 08:40:00 +0700

    nop_bai_tap

--- case_study_csdl/case_study_quan_ly_resort.sql
+++ case_study_csdl/case_study_quan_ly_resort.sql
@@ -176,11 +176,37 @@ insert into hop_dong (ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, ma_nhan_vi
 insert into hop_dong_chi_tiet (so_luong, ma_hop_dong, ma_dich_vu_di_kem) values
 (5,2,4), (8,2,5), (15,2,6), (1,3,1), (11,3,2), (1,1,3), (2,1,2), (2,12,2);
 
+-- 2. Hiển thị thông tin của tất cả nhân viên có trong hệ thống có tên bắt đầu là 
+-- một trong các ký tự “H”, “T” hoặc “K” và có tối đa 15 kí tự.
+select * from nhan_vien
+where (ho_ten like 'H%' or ho_ten like 'T%' or ho_ten like 'K%') and char_length(ho_ten) <= 15;
 
+-- 3. Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+select khach_hang.* , (year(now()) - year(ngay_sinh)) as tuoi
+from khach_hang 
+where ((year(now()) - year(ngay_sinh)) between 18 and 50) and (dia_chi like '% Đà Nẵng' or dia_chi like '% Quảng Trị');
 
+-- 4. Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. 
+-- Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. 
+-- Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”.
+select kh.*, count(hd.ma_khach_hang) as so_lan_dat, lk.ten_loai_khach
+from khach_hang kh
+join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+where ten_loai_khach = 'Diamond'
+group by kh.ma_khach_hang
+order by count(hd.ma_khach_hang) asc;
 
+-- 5. Hiển thị ma_khach_hang, ho_ten, ten_loai_khach, ma_hop_dong, ten_dich_vu, ngay_lam_hop_dong, ngay_ket_thuc, tong_tien 
+-- (Với tổng tiền được tính theo công thức như sau: Chi Phí Thuê + Số Lượng * Giá, với Số Lượng và Giá là từ bảng dich_vu_di_kem, hop_dong_chi_tiet)
+-- cho tất cả các khách hàng đã từng đặt phòng. (những khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+select kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, 
+hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, sum(dv.chi_phi_thue + hdct.so_luong * dvdk.gia) as tong_tien
+from khach_hang kh
+join loai_khach lk on kh.ma_loai_khach = lk.ma_loai_khach
+join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
+join dich_vu dv on hd.ma_dich_vu = dv.ma_dich_vu
+join hop_dong_chi_tiet hdct on hd.ma_hop_dong = hdct.ma_hop_dong
+join dich_vu_di_kem dvdk on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem;
 
-
-
-
-
+		
