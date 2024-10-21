c0e9977129a64998677be193b81d4f57350f1144
unknown <buivanluu1997@gmail.com>
2024-10-21 20:09:07 +0700

    nop_bai_tap

--- ss2_thiet_ke_va_tao_csdl/bai_tap/quan_ly_xuat_nhap_khau.sql
+++ ss2_thiet_ke_va_tao_csdl/bai_tap/quan_ly_xuat_nhap_khau.sql
@@ -1,75 +1,61 @@
-create database if not exists quan_ly_ban_hang;
-use quan_ly_ban_hang;
+create database if not exists quan_ly_xuat_nhap_khau;
+use quan_ly_xuat_nhap_khau;
 
-create table customer (
-id int auto_increment primary key,
-name varchar(25),
-age int 
+create table so_dien_thoai (
+ma_sdt int auto_increment primary key,
+so_dien_thoai varchar(15)
 );
-
-create table `order` (
-id int auto_increment primary key,
-star_date date,
-total_price int, 
-customer_id int,
-foreign key (customer_id) references customer(id)
+create table nha_cc (
+ma_ncc int auto_increment primary key,
+ten_ncc varchar(45),
+dia_chi varchar(45),
+ma_sdt int,
+foreign key (ma_sdt) references so_dien_thoai (ma_sdt)
 );
-
-create table product (
-id int auto_increment primary key,
-name varchar(25),
-price int 
+create table don_dat_hang (
+so_don_hang int auto_increment primary key,
+ngay_dat_hang date,
+ma_ncc int,
+foreign key(ma_ncc) references nha_cc (ma_ncc)
 );
-
-create table order_detail (
-order_id int,
-product_id int,
-quantity int,
-primary key (order_id, product_id),
-foreign key (order_id) references `order` (id),
-foreign key (product_id) references product (id)
+create table vat_tu (
+ma_vat_tu int auto_increment primary key,
+ten_vat_tu varchar(45)
 );
-
-insert into customer (name, age) values ('Minh Quan', 10), ('Ngoc Oanh', 20), ('Hong Ha', 50);
-
-insert into `order` (customer_id, star_date) values (1, '2006/3/21'), (2, '2006/3/23'), (1, '2006/3/16');
-
-insert into product (name, price) values ('May Giat', 3), ('Tu Lanh', 5), ('Dieu Hoa', 7), ('Quat', 1), ('Bep Dien', 2);
-
-insert into order_detail values (1,1,3), (1,3,7), (1,4,2), (2,1,1), (3,1,8), (2,5,4), (2,3,3);
-
--- Hiển thị các thông tin gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
-select od.id, od.star_date, od.total_price
-from `order` od;
-
--- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
-select c.name as customer_name, p.name as product_name
-from customer c
-inner join `order` o on c.id = o.customer_id
-inner join order_detail od on o.id = od.order_id
-inner join product p on od.product_id = p.id;
-
--- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
-select c.name, o.star_date, o.id as order_id
-from customer c
-left join `order` o on c.id = o.customer_id
-where o.id is null;
-
--- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
--- (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn.
--- Giá bán của từng loại được tính = odQTY*pPrice)
-
-select o.id, o.star_date, sum(quantity * price) as total_price
-from `order` o
-inner join order_detail od on o.id = od.order_id
-inner join product p on od.product_id = p.id
-group by o.id;
-
-
-
-
-
-
+create table chi_tiet_don_dat_hang (
+so_don_hang int,
+ma_vat_tu int,
+so_luong int,
+primary key (so_don_hang,ma_vat_tu),
+foreign key(so_don_hang) references don_dat_hang(so_don_hang),
+foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
+);
+create table phieu_xuat (
+so_phieu_xuat int auto_increment primary key,
+ngay_xuat date
+);
+create table chi_tiet_phieu_xuat (
+don_gia_xuat double,
+so_luong_xuat int,
+so_phieu_xuat int,
+ma_vat_tu int,
+primary key (so_phieu_xuat,ma_vat_tu),
+foreign key(so_phieu_xuat) references phieu_xuat(so_phieu_xuat),
+foreign key(ma_vat_tu) references vat_tu(ma_vat_tu)
+);
+create table phieu_nhap (
+so_phieu_nhap int auto_increment primary key,
+ngay_nhap date
+);
+create table chi_tiet_phieu_nhap (
+don_gia_nhap double,
+so_luong_nhap int,
+ma_vat_tu int,
+so_phieu_nhap int,
+primary key(ma_vat_tu, so_phieu_nhap),
+foreign key(ma_vat_tu) references vat_tu (ma_vat_tu),
+foreign key(so_phieu_nhap) references phieu_nhap (so_phieu_nhap)
+);
 
 
 
